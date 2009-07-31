88bfbfb395e4ffef859780e41fb926c9f6ab0aa2
akarnokd@gmail.com <akarnokd@gmail.com@348752c8-768c-11de-a560-a546dc0df6aa>
2009-07-31 13:25:18 +0000

    Rename tab

--- open-so-frontend/src/hu/openso/frontend/TitleWithClose.java
+++ open-so-frontend/src/hu/openso/frontend/TitleWithClose.java
@@ -10,6 +10,7 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.border.EtchedBorder;
@@ -18,15 +19,37 @@ public class TitleWithClose extends JPanel {
 	private static final long serialVersionUID = 2314996247881493085L;
 	private final Component component;
 	private final JTabbedPane tabbed;
+	private String title;
+	private JLabel comp;
 
 	public TitleWithClose(String title, JTabbedPane tab, Component c) {
+		this.title = title;
 		setOpaque(false);
 		this.tabbed = tab;
 		this.component = c;
 		setBorder(BorderFactory.createEmptyBorder(3, 0, 0, 2));
 		setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
-		add(new JLabel(title));
+		comp = new JLabel(title);
+		add(comp);
 		add(Box.createHorizontalStrut(5));
+		
+		JButton btnR = new JButton("R");
+		btnR.setPreferredSize(new Dimension(17, 17));
+		btnR.setToolTipText("Rename this tab");
+		btnR.setFocusable(false);
+		btnR.setBorderPainted(false);
+		btnR.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+		btnR.setRolloverEnabled(true);
+		btnR.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				doRename();
+			}
+		});
+		add(btnR);
+		add(Box.createHorizontalStrut(5));
+		
+		
 		JButton btn = new JButton("X");
 		btn.setPreferredSize(new Dimension(17, 17));
 		btn.setToolTipText("Close this tab");
@@ -37,13 +60,32 @@ public class TitleWithClose extends JPanel {
 		btn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				int i = tabbed.indexOfComponent(component);
-				if (i > 0) {
-					tabbed.setSelectedIndex(i - 1);
-				}
-				tabbed.removeTabAt(i);
+				doCloseTab();
 			}
 		});
 		add(btn);
+	}
+
+	protected void doRename() {
+		String value = JOptionPane.showInputDialog(this, "Please enter the name of the tab");
+		if (value != null) {
+			setTitle(value);
+		}
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+		comp.setText(title);
+	}
+
+	private void doCloseTab() {
+		int i = tabbed.indexOfComponent(component);
+		if (i > 0) {
+			tabbed.setSelectedIndex(i - 1);
+		}
+		tabbed.removeTabAt(i);
 	}
 }
